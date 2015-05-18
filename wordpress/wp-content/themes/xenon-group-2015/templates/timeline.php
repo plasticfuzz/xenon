7d240e54468350d04a58434aa5db498e805ab1ba
Phil <phil@fyll.co.uk>
2015-05-18 20:26:40 +0100

    timeline template and styles

--- wordpress/wp-content/themes/xenon-group-2015/templates/timeline.php
+++ wordpress/wp-content/themes/xenon-group-2015/templates/timeline.php
@@ -21,17 +21,41 @@ get_header(); $Px = 'tl_'; ?>
       <?php $lead_p = get_field( ($Px.'lead_paragraph') );
       if ($lead_p) : ?>        
         <div class="pure-g wrapper-1140">  
-          <?php // Our Team - Leading  ?><div class="lead-box pure-u-1">
+          <?php // Our Team - Leading  ?><div class="lead-box text-gray pure-u-1">
             <?php echo $lead_p; ?>
           </div>
         </div><?php
       endif; ?>
-
-      <div class="pure-g wrapper-1140">
-        <div class="pure-u-1">
-          <?php the_field( ($Px.'title') ); ?>
-        </div>
-      </div>    
+      
+      <?php // Timeline - Posts
+      if ( have_rows( ($Px.'posts') ) ) : ?>
+          <div class="pure-g wrapper-1140">
+            <div class="pure-u-1">
+              <h2><?php the_field( ($Px.'title') ); ?></h2>
+            </div>          
+            <div class="pure-u-1">
+              <div class="timeline">
+                <?php while ( have_rows( ($Px.'posts') ) ) : the_row();
+                  $side = get_sub_field( ($Px.'post_position') ) ?>
+                  <div class="timeline__block">
+                    <div class="timeline__block__date">
+                      <span><?php the_sub_field( ($Px.'post_date') ) ?></span>
+                    </div>      
+                    <div class="timeline__block__content
+                      <?php echo ($side === 'left') ? 'f-left' : 'f-right' ?>">
+                      <?php the_sub_field( ($Px.'post') ) ?> 
+                    </div>
+                  </div>
+                <?php endwhile; ?>
+                <div class="timeline__block">
+                  <div class="timeline__block__onwards">
+                    <span>Onwards</span>
+                  </div> 
+                </div>
+              </div>
+            </div>
+          </div><?php
+      endif ?>
     <?php endwhile ?>     
   </main>
 <?php get_footer();
